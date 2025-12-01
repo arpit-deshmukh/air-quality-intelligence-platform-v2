e73e7097a42f46183b0877785bd1ef17fe763f24
Arpit Deshmukh <arpitdeshmukh21@gmail.com>
2025-12-01 19:03:31 +0530

    temp name changed

--- frontend/src/pages/AQIInfo/InfoCard.jsx
+++
@@ -1,8 +0,0 @@
-export default function InfoCard({ title, children }) {
-  return (
-    <div className="bg-white border rounded-xl p-5 shadow-sm">
-      <h3 className="text-xl text-blue-900 font-semibold mb-3">{title}</h3>
-      {children}
-    </div>
-  );
-}
